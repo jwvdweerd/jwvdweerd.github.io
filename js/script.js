9b319fd026e3907fcb36175744326319ac186efd
Rogier van der Weerd <rogerscode@gmail.com>
2025-01-26 11:39:35 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,7 +75,7 @@ function openModal(record) {
     thumbnails.forEach((thumbnail, index) => {
         if (thumbnail.endsWith('.pdf')) {
             content += `<div class="pdf-thumbnail" onclick="openHighResImage(${index})">
-                            <iframe src="${thumbnail}" width="150" height="200"></iframe>
+                            <embed src="${thumbnail}" type="application/pdf" width="150" height="200">
                         </div>`;
         } else {
             content += `<img src="${thumbnail}" alt="${title} Thumbnail ${index + 1}" class="thumbnail" onclick="openHighResImage(${index})">`;
@@ -96,7 +96,7 @@ function openHighResImage(index) {
     //highResImage.src = highResImages[currentHighResIndex];
     highResModal.style.display = 'block';
     if (highResImages[currentHighResIndex].endsWith('.pdf')) {
-        highResContent.innerHTML = `<iframe src="${highResImages[currentHighResIndex]}" width="100%" height="100%"></iframe>`;
+        highResContent.innerHTML = `<embed src="${highResImages[currentHighResIndex]}" type="application/pdf" width="100%" height="100%">`;
     } else {
         highResContent.innerHTML = `<img src="${highResImages[currentHighResIndex]}" alt="High Resolution Image" style="max-height: 100%; max-width: 100%; object-fit: contain;">`;
     }
